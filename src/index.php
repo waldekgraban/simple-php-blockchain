6a81a1a41625efc76a61b2175d70561f45478c8c
Waldemar Graban <waldekgraban@gmail.com>
2022-06-04 00:23:53 +0200

    Add files via upload

--- src/index.php
+++ src/index.php
@@ -9,30 +9,11 @@ namespace Waldekgraban\SimplePhpBlockchain;
 
 require_once "../vendor/autoload.php";
 
-use Waldekgraban\SimplePhpBlockchain\Blockchain\Block;
+use Waldekgraban\SimplePhpBlockchain\Mine\Miner;
 
 //example of use
 
-$blockchain = new Block();
+$miner = new Miner();
 
-function mine() {
-    global $blockchain;
-    $previous_block = $blockchain->print_previous_block();
-    $previous_proof = $previous_block['proof'];
-
-    $proof = $blockchain->proof_of_work($previous_proof);
-    $previous_hash = $blockchain->hash($previous_block);
-    $block = $blockchain->create_block($proof, $previous_hash);
-
-    // var_dump($block);
-    // die;
-
-    echo "New Block has been created\n";
-    echo "Index: " . $block['index'] . "\n";
-    echo "Timestamp: " . $block['timestamp'] . "\n";
-    echo "Proof: " . $block['proof'] . "\n";
-    echo "Previous Hash: " . $block['previous_hash'] . "\n";
-}
-
-mine();
-mine();
+$miner->mine();
+$miner->mine();
