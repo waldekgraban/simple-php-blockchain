36dabed1406a264017d133e7735bc7e571e09e3b
Waldemar Graban <waldekgraban@gmail.com>
2022-06-08 21:13:04 +0200

    Update Miner.php

--- src/Mine/Miner.php
+++ src/Mine/Miner.php
@@ -14,7 +14,7 @@ class Miner
 	/**
 	 * Example usage of blocks - show block info
 	 */
-	public function mine()
+	public function mineOnce()
 	{
 		$block = $this->mine->createBlock();
 
@@ -23,5 +23,35 @@ class Miner
 	    echo "Timestamp: " . $block['timestamp'] . "<br>";
 	    echo "Proof: " . $block['proof'] . "<br>";
 	    echo "Previous Hash: " . $block['previous_hash'] . "<br><br>";
+	}
+
+	public function mine()
+	{
+		
+		for ($i=0; $i < $this->getQuantityOfBlock() +1; $i++) { 
+		
+			$block = $this->mine->createBlock();
+
+			echo "New Block has been created" . "<br>";
+		    echo "Index: " . $block['index'] . "<br>";
+		    echo "Timestamp: " . $block['timestamp'] . "<br>";
+		    echo "Proof: " . $block['proof'] . "<br>";
+		    echo "Previous Hash: " . $block['previous_hash'] . "<br><br>";
+		}
+		
+	}
+
+	public function setQuantityOfBlock(int $blockQty): void
+	{
+		$this->blockQty = $blockQty;
+	}
+
+	public function getQuantityOfBlock(): int
+	{
+		if(isset($this->blockQty)){
+			return $this->blockQty;
+		}
+
+		return 1;
 	}
 }
